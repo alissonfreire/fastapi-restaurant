f890e10ec4003c7e5393a87b5bb7c1aee8da951a
Alisson Freire <alissonfreire2@gmail.com>
2024-07-22 18:28:16 -0300

    chore: added initial test for app

---
+++ tests/main_test.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_root_should_returns_ok_status():
+    client = TestClient(app)
+
+    response = client.get("/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"hello": "world"}
