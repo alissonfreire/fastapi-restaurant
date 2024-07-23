e937e1228b9beeffc2631ba2852007a3d6fbd57e
Alisson Freire <alissonfreire2@gmail.com>
2024-07-23 19:49:11 -0300

    feat: added user_schema and user_repository

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserCreateInput(BaseModel):
+    username: str = Field(min_length=3, max_length=255)
+    email: EmailStr = Field()
+    password: str = Field(min_length=8, max_length=255)
+
+
+class UserUpdateInput(BaseModel):
+    username: str | None = Field(min_length=3, max_length=255)
+    email: EmailStr | None = Field()
+    password: str | None = Field(min_length=8, max_length=255)
