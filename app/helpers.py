54efebb71f9523fb2144716b76098fe4c4c845de
Alisson Freire <alissonfreire2@gmail.com>
2024-07-23 11:40:59 -0300

    chore: setup env vars and add load_env helper

---
+++ app/helpers.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import find_dotenv, load_dotenv
+
+def load_env():
+    load_dotenv(find_dotenv())
+
+    if os.getenv('ENV', 'dev') == 'test':
+        load_dotenv(find_dotenv('.env.testing'), override=True)
