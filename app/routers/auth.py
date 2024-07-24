3ed55d4d7ce31415cb28ea9b6fa007013349b6e7
Alisson Freire <alissonfreire2@gmail.com>
2024-07-24 17:55:44 -0300

    chore: added get_current_user function on auth router

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,6 +5,7 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
 from app.config.database import get_session
+from app.models.user import User
 from app.schemas.response_schema import SuccessResponse
 from app.schemas.user_schema import UserCreateInput, UserLoginInput, UserPublic
 from app.services.user_service import UserService
@@ -12,6 +13,22 @@ from app.services.user_service import UserService
 router = APIRouter(prefix='/auth', tags=['auth'])
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: Session = Depends(get_session),
+):
+    user_service = UserService(session=session)
+
+    user = user_service.get_user_from_token(access_token=token)
+
+    if user is None:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+        )
+
+    return user
 
 
 @router.post(
@@ -58,17 +75,5 @@ def login_user(data: UserLoginInput, session: Session = Depends(get_session)):
     status_code=HTTPStatus.OK,
     response_model=SuccessResponse,
 )
-def me(
-    token: str = Depends(oauth2_scheme),
-    session: Session = Depends(get_session),
-):
-    user_service = UserService(session=session)
-
-    user = user_service.get_user_from_token(access_token=token)
-
-    if user is None:
-        raise HTTPException(
-            status_code=HTTPStatus.UNAUTHORIZED,
-        )
-
+def me(user: User = Depends(get_current_user)):
     return SuccessResponse(data={'user': UserPublic(**user.__dict__)})
